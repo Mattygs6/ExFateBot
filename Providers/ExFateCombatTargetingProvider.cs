a28cf976ba61f2ea753f1afc3d074f730260d866
Matt Gramolini <mattygs@gmail.com>
2016-03-25 13:47:09 -0400

    rework targeting so that we don't keep running in and out of fates when level synced.

--- Providers/ExFateCombatTargetingProvider.cs
+++ Providers/ExFateCombatTargetingProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace ExFateBot
 {
+	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 
@@ -65,6 +66,11 @@
 				return false;
 			}
 
+			if (Core.Player.IsLevelSynced && IsOutOfRangeOfLevelSyncedFate(battleCharacter))
+			{
+				return false;
+			}
+
 			if (this.attackers.Contains(battleCharacter))
 			{
 				return true;
@@ -88,38 +94,51 @@
 			return !inCombat;
 		}
 
+		private bool IsOutOfRangeOfLevelSyncedFate(BattleCharacter battleCharacter)
+		{
+			// return true if distance from middle of fate to enemy is greater than
+			var isOutOfRange = ExFateBot.FateData.Location.Distance(battleCharacter.Location) 
+			> (ExFateBot.FateData.Radius * 0.8f) // 80% of the fate radius plus
+			+ RoutineManager.Current.PullRange // player pull range from combat routine
+			+ battleCharacter.CombatReach; // distance(radius) from enemy in which your abilities calculate their distance.
+
+			return isOutOfRange;
+		}
+
 		private double GetWeight(BattleCharacter battleCharacter)
 		{
-			var weight = battleCharacter.Distance() * -30 + 1800;
+			// Math.Max(battleCharacter.Distance() - battleCharacter.CombatReach, 0f) prevents enemies from walking on top of us becoming a lot more important than just using Distance();
+			// Weight by distance [0 = 2000, 50 = 0];
+			var weight = Math.Max(battleCharacter.Distance() - battleCharacter.CombatReach, 0f)  * -40 + 2000;
 
+			// we are targetting the enemy, add 150
 			if (battleCharacter.Pointer == Core.Player.PrimaryTargetPtr)
 			{
 				weight += 150;
 			}
 
+			// the enemy is targetting us, add 50, add (100 - %hp) times 2
 			if (battleCharacter.HasTarget && battleCharacter.CurrentTargetId == Core.Player.ObjectId)
 			{
-				weight += 50;
+				weight += 50 + (100f - battleCharacter.CurrentHealthPercent) * 2;
 			}
 
+			// enemy is part of our fate, add 190
 			if (ExFateBot.FateData != null && battleCharacter.FateId == ExFateBot.FateData.Id)
 			{
-				weight += 210;
+				weight += 190;
 			}
 
+			// enemy is on our attackers list, add 60
 			if (this.attackers.Contains(battleCharacter))
 			{
-				weight += 110;
+				weight += 60;
 			}
 
-			if (battleCharacter.CurrentTargetId == Core.Player.ObjectId)
-			{
-				weight += (100f - battleCharacter.CurrentHealthPercent);
-			}
-
+			// enemy is not in combat, subtract 80
 			if (!battleCharacter.InCombat)
 			{
-				weight += 130;
+				weight -= 80;
 			}
 
 			return weight;
